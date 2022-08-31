382718fe1f8504d5f87257ce9a96a5206fed041a
Dokkabei97 <wkdrn970@naver.com>
2022-08-31 15:37:15 +0900

    feat: 'user 구조체, wallet 구조체'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,64 @@
-use std::net::TcpListener;
-
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+}
+
+pub mod user {
+    use crate::wallet::Wallet;
+
+    pub struct User {
+        pub user_id: i32,
+        pub point: i32,
+        pub wallet_address: String,
+    }
+
+    impl User {
+        pub fn new(user_id: i32) -> User {
+            let wallet_address = Wallet::new(user_id);
+            User {
+                user_id,
+                point: 100,
+                wallet_address,
+            }
+        }
+    }
+}
+
+pub mod wallet {
+    pub struct Wallet {
+        wallet_id: i32,
+        user_id: i32,
+        wallet_address: String,
+    }
+
+    impl Wallet {
+        pub fn new(user_id: i32) -> String {
+            let wallet = Wallet {
+                wallet_id: user_id,
+                user_id,
+                wallet_address: "123".to_string()
+            };
+            wallet.wallet_address
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::user::User;
+    use crate::wallet::Wallet;
+
+    #[test]
+    fn create_wallet_success() {
+        let wallet = Wallet::new(1);
+        assert_eq!(wallet, "123".to_string());
+    }
+
+    #[test]
+    fn create_user_success() {
+        let user = User::new(1);
+        assert_eq!(user.user_id, 1);
+        assert_eq!(user.point, 100);
+        assert_eq!(user.wallet_address, "123".to_string());
+    }
 
 }
--- src/user/user.rs
+++ src/user/user.rs
@@ -1,25 +1,27 @@
 pub mod user {
-    struct User {
-        user_id: i32,
-        point: i32,
-        wallet_address: String,
+    pub struct User {
+        pub user_id: i32,
+        pub point: i32,
+        pub wallet_address: String,
     }
 
-    pub fn create_user(user_id: i32) -> User {
-        User {
-            user_id,
-            point: 1000,
-            wallet_address: "123",
+    impl User {
+        pub fn new(user_id: i32) -> User {
+            User {
+                user_id,
+                point: 100,
+                wallet_address: "123",
+            }
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-
+    use user;
     #[test]
     fn create_user_success() {
-
-        assert_eq!();
+        let user = User::new(1);
+        assert_eq!(user::user_id, 2);
     }
 }
