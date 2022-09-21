0f661e69faca952dacb69b61986df2f2606ef115
syh39 <syh-30@naver.com>
2022-09-21 17:41:02 +0900

    사용자 로그인 먼저하기 / 입력시 개행문자 제거

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::io::Write; // bring flush() into scope
 
 const IP_ADDRESS: &str = "localhost:7878";
 
 fn return_user_input() -> String {
     let mut input = String::new();
 
+    io::stdout().flush().unwrap();
     io::stdin().read_line(&mut input)
         .expect("Failed to read line");
 
     input
 }
 fn main() -> std::io::Result<()> {
+    let mut user_name;
+
+
     println!("==================== WELCOME TO VCTG ====================");
     loop {
         let mut stream = TcpStream::connect(IP_ADDRESS)?;
-        println!("---------- Select Action (Press Q or q to quit) ----------");
+
+        println!("--------------------- Select Action ----------------------");
         println!("| [1] Register VCTG                                      |");
         println!("| [2] Sign In VCTG                                       |");
-        println!("| [3] Check Wallet                                       |");
-        println!("| [4] Mine Coin                                          |");
-        println!("| [5] Sell Coin                                          |");
-        println!("| [6] Buy Coin                                           |");
         println!("----------------------------------------------------------");
 
         let mut guess = String::new();
@@ -30,19 +32,72 @@ fn main() -> std::io::Result<()> {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
+        if guess.trim()=="1" {
+            println!("You selected Register VCTG");
+            println!("Enter ID : ");
+
+            user_name = return_user_input();
+            user_name = user_name.trim().to_string();
+            let str = format!("register|{}|", user_name);
+
+            let writeBuffer = str.as_bytes();
+            stream.write(writeBuffer)?;
+
+            let mut readBuffer = [0; 512];
+            stream.read(&mut readBuffer).unwrap();
+
+            println!("Response: {}", String::from_utf8_lossy(&readBuffer[..]));
+            break;
+        }
+
+        else if guess.trim()=="2" {
+            println!("You selected Sign In VCTG");
+            println!("Enter ID : ");
+
+            user_name = return_user_input();
+            user_name = user_name.trim().to_string();
+            let str = format!("signin|{}|", user_name);
+
+            let writeBuffer = str.as_bytes();
+            stream.write(writeBuffer)?;
+
+            let mut readBuffer = [0; 512];
+            stream.read(&mut readBuffer).unwrap();
+
+            println!("Response: {}", String::from_utf8_lossy(&readBuffer[..]));
+            break;
+        }
+
+        else {
+            println!("Enter Again");
+        }
+    }
+    loop {
+        let mut stream = TcpStream::connect(IP_ADDRESS)?;
+        println!("---------- Select Action (Press Q or q to quit) ----------");
+        println!("| [1] Check Wallet                                       |");
+        println!("| [2] Mine Coin                                          |");
+        println!("| [3] Sell Coin                                          |");
+        println!("| [4] Buy Coin                                           |");
+        println!("----------------------------------------------------------");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+
         if guess.trim()=="Q" || guess.trim()=="q" {
             println!("Bye!");
             println!("===============================================");
             break;
         }
 
+
         if guess.trim()=="1" {
-            println!("You selected Register VCTG");
-            println!("Enter ID : ");
+            println!("You selected Check Wallet");
 
-            let id = return_user_input();
-            let str = format!("register|{}|", id);
-
+            let str = format!("wallet|{}|", user_name);
             let writeBuffer = str.as_bytes();
             stream.write(writeBuffer)?;
 
@@ -53,12 +108,9 @@ fn main() -> std::io::Result<()> {
         }
 
         else if guess.trim()=="2" {
-            println!("You selected Sign In VCTG");
-            println!("Enter ID : ");
+            println!("You selected Mine Coin");
 
-            let id = return_user_input();
-            let str = format!("signin|{}|", id);
-
+            let str = format!("mining|{}|", user_name);
             let writeBuffer = str.as_bytes();
             stream.write(writeBuffer)?;
 
@@ -69,8 +121,10 @@ fn main() -> std::io::Result<()> {
         }
 
         else if guess.trim()=="3" {
-            println!("You selected Check Wallet");
-            let writeBuffer = b"wallet|user|";
+            println!("You selected Sell Coin");
+
+            let str = format!("sell|{}|", user_name);
+            let writeBuffer = str.as_bytes();
             stream.write(writeBuffer)?;
 
             let mut readBuffer = [0; 512];
@@ -80,8 +134,10 @@ fn main() -> std::io::Result<()> {
         }
 
         else if guess.trim()=="4" {
-            println!("You selected Mine Coin");
-            let writeBuffer = b"mining|user|";
+            println!("You selected Buy Coin");
+
+            let str = format!("buy|{}|", user_name);
+            let writeBuffer = str.as_bytes();
             stream.write(writeBuffer)?;
 
             let mut readBuffer = [0; 512];
@@ -90,13 +146,9 @@ fn main() -> std::io::Result<()> {
             println!("Response: {}", String::from_utf8_lossy(&readBuffer[..]));
         }
 
-        else if guess.trim()=="5" {
-            println!("You selected Sell Coin");
+        else {
+            ;
         }
-
-        else if guess.trim()=="6" {
-            println!("You selected Buy Coin");
-        } // TODO : TCP 연결 체크
     }
     Ok(())
 }
